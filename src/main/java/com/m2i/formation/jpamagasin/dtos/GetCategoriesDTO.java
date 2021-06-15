02dcaee785c6aa8437972c5629711a9dee4acc3b
Najim <nelfateoui@gmail.com>
2021-06-15 18:20:34 +0200

    05 Test quasi ok pour tous, certains DTO à reprendre

--- src/main/java/com/m2i/formation/jpamagasin/dtos/GetCategoriesDTO.java
+++ src/main/java/com/m2i/formation/jpamagasin/dtos/GetCategoriesDTO.java
@@ -2,9 +2,11 @@ package com.m2i.formation.jpamagasin.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GetCategoriesDTO {
 	
 	private String nom;
